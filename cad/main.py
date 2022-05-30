9057aa3bdbb8535fdd0c4193953e879b4066f969
Adam Simpkins <adam@adamsimpkins.net>
2022-05-30 13:51:54 -0700

    [cad] minor tweaks to blender script for use from the CLI

--- cad/main.py
+++ cad/main.py
@@ -73,13 +73,6 @@ def gen_keyboard(kbd: oukey.Keyboard) -> bpy.types.Object:
     bevel.limit_method = "WEIGHT"
     bevel.segments = 8
 
-    # Adjust the camera to better show the keyboard
-    layout = bpy.data.screens["Layout"]
-    view_areas = [a for a in layout.areas if a.type == "VIEW_3D"]
-    for a in view_areas:
-        region = a.spaces.active.region_3d
-        region.view_distance = 350
-
     # Apply the bevel modifier
     apply_bevel = True
     if apply_bevel:
@@ -387,27 +380,34 @@ def do_main() -> None:
     print("done")
 
 
+def command_line_main() -> None:
+    try:
+        do_main()
+
+        # Adjust the camera to better show the keyboard
+        layout = bpy.data.screens["Layout"]
+        view_areas = [a for a in layout.areas if a.type == "VIEW_3D"]
+        for a in view_areas:
+            region = a.spaces.active.region_3d
+            region.view_distance = 350
+    except Exception as ex:
+        import logging
+
+        logging.exception(f"unhandled exception: {ex}")
+        sys.exit(1)
+
+
 def main() -> None:
     # If we are being run as a script on the command line,
     # then exit on error rather than let blender finish opening even though we
     # didn't run correctly.  We don't want this behavior when being run
     # on-demand in an existing blender session, though.
-    exit_on_error = False
     for arg in sys.argv:
         if arg == "-P" or arg == "--python":
-            exit_on_error = True
-            break
+            command_line_main()
+            return
 
-    if exit_on_error:
-        try:
-            do_main()
-        except Exception as ex:
-            import logging
-
-            logging.exception(f"unhandled exception: {ex}")
-            sys.exit(1)
-    else:
-        do_main()
+    do_main()
 
 
 if __name__ == "__main__":
