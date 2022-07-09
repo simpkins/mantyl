e35a3aaca12dd8207fb06b10c60cae1eabd85153
Adam Simpkins <adam@adamsimpkins.net>
2022-07-09 12:08:34 -0700

    [cad] rework the OLED display cutout

--- cad/mantyl/oled_holder.py
+++ cad/mantyl/oled_holder.py
@@ -5,119 +5,125 @@
 
 from __future__ import annotations
 
+from . import cad
 from . import blender_util
 
 import bpy
 from typing import Tuple
 
 
-def oled_holder_parts(
-    wall_thickness: float
-) -> Tuple[bpy.types.Object, bpy.types.Object]:
-    display_w = 32.5
+def oled_holder_parts(wall_thickness: float) -> bpy.types.Object:
+    # The display is not centered on the PCB face.
+    # Make the display centered, and the PCB offset a little bit.
+    display_offset = 2.5 * 0.5
+
+    display_w = 30
     display_h = 12.2
-    extra_thickness = 0.1
-    display_thickness = 1.53 + extra_thickness
+    display_thickness = 1.53 + 0.1
 
     pcb_w = 33.5
     pcb_h = 21.8
     pcb_thickness = 1.57
     pcb_tolerance = 0.5
 
-    qt_cable_h = 9
-    qt_cable_cutout_w = 3
-
-    display_cutout = blender_util.cube(display_w, display_thickness, display_h)
-    with blender_util.TransformContext(display_cutout) as ctx:
-        ctx.translate(0, (display_thickness - extra_thickness) / 2, 0)
-    pcb_cutout = blender_util.cube(
-        pcb_w + pcb_tolerance, wall_thickness, pcb_h + pcb_tolerance
+    display_x_r = display_w * 0.5
+    display_h_r = display_h * 0.5
+    display_cutout = blender_util.range_cube(
+        (-display_x_r, display_x_r),
+        (0.0, display_thickness),
+        (-display_h_r, display_h_r),
     )
-    with blender_util.TransformContext(pcb_cutout) as ctx:
-        ctx.translate(0, (wall_thickness / 2) + display_thickness - 0.1, 0)
-    qt_cable_cutout = blender_util.cube(
-        qt_cable_cutout_w + extra_thickness, wall_thickness, qt_cable_h
+
+    pcb_x_r = (pcb_w + pcb_tolerance) * 0.5
+    pcb_h_r = (pcb_h + pcb_tolerance) * 0.5
+    pcb_cutout = blender_util.range_cube(
+        (-pcb_x_r + display_offset, pcb_x_r + display_offset),
+        (display_thickness, wall_thickness),
+        (-pcb_h_r, pcb_h_r),
     )
-    with blender_util.TransformContext(qt_cable_cutout) as ctx:
-        ctx.translate(
-            -(qt_cable_cutout_w + pcb_w) / 2,
-            (wall_thickness / 2) + display_thickness - 0.1,
-            0,
-        )
 
-    header_cutout = blender_util.cube(18, wall_thickness, 4)
-    with blender_util.TransformContext(header_cutout) as ctx:
-        ctx.translate(0, (wall_thickness / 2) + 0.8, 2 + (-pcb_h / 2))
-
-    cutin_w = 2.5
-    cutin_h = 2.0
-    cutin_thickness = display_thickness - 0.1
-    cable_cutin = blender_util.cube(cutin_w, cutin_thickness, cutin_h)
-    with blender_util.TransformContext(cable_cutin) as ctx:
-        ctx.translate(
-            (display_w - cutin_w) / 2,
-            cutin_thickness / 2,
-            (display_h - cutin_h) / 2.0,
-        )
-
-    oled_cable_h = display_h - cutin_h
-    oled_cable_cutout_w = 2
-    oled_cable_cutout = blender_util.cube(
-        oled_cable_cutout_w + extra_thickness,
-        wall_thickness + extra_thickness,
-        oled_cable_h,
+    header_cutout_w = 18.0
+    header_cutout = blender_util.range_cube(
+        (
+            header_cutout_w * -0.5 + display_offset,
+            header_cutout_w * 0.5 + display_offset,
+        ),
+        (1.0, display_thickness),
+        (-pcb_h_r + 0.25, -pcb_h_r + 0.25 + 4.0),
     )
-    oled_cable_cutout_diff = blender_util.cube(
-        10, 2, oled_cable_h + extra_thickness
+
+    qt_cutout_h = 9
+    qt_cutout_w = 3
+    qt_cable_cutout = blender_util.range_cube(
+        (-pcb_x_r + display_offset, -pcb_x_r + display_offset - qt_cutout_w),
+        (display_thickness, wall_thickness),
+        (qt_cutout_h * -0.5, qt_cutout_h * 0.5),
     )
-    with blender_util.TransformContext(oled_cable_cutout_diff) as ctx:
-        ctx.translate(0, -1, 0)
-        ctx.rotate(15, "Z")
-        ctx.translate(-1, 1 + (-(2 + wall_thickness) / 2), 0)
-    blender_util.difference(oled_cable_cutout, oled_cable_cutout_diff)
-    with blender_util.TransformContext(oled_cable_cutout) as ctx:
-        ctx.translate(
-            (oled_cable_cutout_w + display_w) / 2,
-            (wall_thickness + extra_thickness) / 2,
-            -(cutin_h / 2),
-        )
 
-    if False:
-        stud = (
-            standoff_stud(d=2.5)
-            .rotate(-90, 0, 0)
-            .translate(0, display_thickness - 0.01, 0)
-        )
-        postive_parts = [
-            stud.translate(-14.0, 0, -8.325),
-            stud.translate(14.0, 0, -8.325),
-            stud.translate(14.0, 0, 8.175),
-            stud.translate(-14.0, 0, 8.175),
-            cable_cutin,
-        ]
-        pos = Shape.union(postive_parts)
-    else:
-        pos = cable_cutin
+    oled_cutout_x = 1.0
+    mesh = cad.Mesh()
+    f_bl = mesh.add_xyz(display_x_r, 0.2, -display_h_r)
+    f_tl = mesh.add_xyz(display_x_r, 0.2, display_h_r - 2.0)
+    b_bl = mesh.add_xyz(display_x_r, wall_thickness, -display_h_r)
+    b_tl = mesh.add_xyz(display_x_r, wall_thickness, display_h_r - 2.0)
 
-    neg = display_cutout
-    blender_util.union(neg, pcb_cutout)
-    blender_util.union(neg, qt_cable_cutout)
-    blender_util.union(neg, oled_cable_cutout)
-    blender_util.union(neg, header_cutout)
+    f_br = mesh.add_xyz(
+        pcb_x_r + display_offset + oled_cutout_x,
+        display_thickness,
+        -display_h_r,
+    )
+    f_tr = mesh.add_xyz(
+        pcb_x_r + display_offset + oled_cutout_x,
+        display_thickness,
+        display_h_r - 2.0,
+    )
+    b_br = mesh.add_xyz(
+        pcb_x_r + display_offset + oled_cutout_x, wall_thickness, -display_h_r
+    )
+    b_tr = mesh.add_xyz(
+        pcb_x_r + display_offset + oled_cutout_x,
+        wall_thickness,
+        display_h_r - 2.0,
+    )
 
-    return neg, pos
+    mesh.add_quad(f_tl, f_bl, b_bl, b_tl)
+    mesh.add_quad(b_tr, b_tl, b_bl, b_br)
+    mesh.add_quad(f_tr, b_tr, b_br, f_br)
+    mesh.add_quad(f_tl, f_tr, f_br, f_bl)
+    mesh.add_quad(f_tl, b_tl, b_tr, f_tr)
+    mesh.add_quad(f_bl, f_br, b_br, b_bl)
+    oled_cable_cutout = blender_util.new_mesh_obj("oled_cable_cutout", mesh)
+
+    blender_util.union(display_cutout, pcb_cutout)
+    blender_util.union(display_cutout, header_cutout)
+    blender_util.union(display_cutout, qt_cable_cutout)
+    blender_util.union(display_cutout, oled_cable_cutout)
+
+    # At the moment, don't bother with studs for the PCB screw holes
+    return display_cutout
+
+    stud_positions = [
+        (-14.0, -8.325),
+        (14.0, -8.325),
+        (14.0, 8.175),
+        (-14.0, 8.175),
+    ]
+    stud_r = 1.25 * 0.85
+    for (x, z) in stud_positions:
+        stud_h = wall_thickness - display_thickness
+        stud = blender_util.cylinder(r=stud_r, h=stud_h, fn=32)
+        with blender_util.TransformContext(stud) as ctx:
+            ctx.rotate(90, "X")
+            ctx.translate(x, (stud_h * 0.5) + display_thickness, z)
+
+        blender_util.difference(display_cutout, stud)
+
+    return display_cutout
 
 
 def test() -> bpy.types.Object:
-    wall = blender_util.range_cube((-25, 25), (0.0, 4.0), (0.0, 40.0))
-    neg, pos = oled_holder_parts(4.0)
-
-    with blender_util.TransformContext(neg) as ctx:
-        ctx.translate(0, 0, 20)
-    with blender_util.TransformContext(pos) as ctx:
-        ctx.translate(0, 0, 20)
-
+    wall = blender_util.range_cube((-25, 25), (0.0, 4.0), (-20.0, 20.0))
+    neg = oled_holder_parts(4.0)
     blender_util.difference(wall, neg)
-    blender_util.union(wall, pos)
 
     return wall
