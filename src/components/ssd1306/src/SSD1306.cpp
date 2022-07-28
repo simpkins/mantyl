082dcb253af4f960579bcf2fe517261c81c1ce49
Adam Simpkins <adam@adamsimpkins.net>
2022-07-27 23:13:17 -0700

    [firmware] implement lower case letters for the font

--- src/components/ssd1306/src/SSD1306.cpp
+++ src/components/ssd1306/src/SSD1306.cpp
@@ -44,6 +44,33 @@ const uint8_t font_data_X[] = {0x63, 0x14, 0x08, 0x14, 0x63};
 const uint8_t font_data_Y[] = {0x07, 0x08, 0x70, 0x08, 0x07};
 const uint8_t font_data_Z[] = {0x61, 0x51, 0x49, 0x45, 0x43};
 
+const uint8_t font_data_a[] = {0x20, 0x54, 0x54, 0x54, 0x78};
+const uint8_t font_data_b[] = {0x7f, 0x44, 0x44, 0x44, 0x38};
+const uint8_t font_data_c[] = {0x38, 0x44, 0x44, 0x44, 0x28};
+const uint8_t font_data_d[] = {0x38, 0x44, 0x44, 0x44, 0x7f};
+const uint8_t font_data_e[] = {0x38, 0x54, 0x54, 0x54, 0x08};
+const uint8_t font_data_f[] = {0x08, 0x7e, 0x09, 0x09};
+const uint8_t font_data_g[] = {0x18, 0xa4, 0xa4, 0xa4, 0x7c};
+const uint8_t font_data_h[] = {0x7f, 0x04, 0x04, 0x04, 0x78};
+const uint8_t font_data_i[] = {0x7a, 0x40};
+const uint8_t font_data_j[] = {0x40, 0x80, 0x84, 0x7d};
+const uint8_t font_data_k[] = {0x7f, 0x10, 0x28, 0x44};
+const uint8_t font_data_l[] = {0x7f, 0x40};
+const uint8_t font_data_m[] = {0x7c, 0x04, 0x78, 0x04, 0x78};
+const uint8_t font_data_n[] = {0x7c, 0x04, 0x04, 0x78};
+const uint8_t font_data_o[] = {0x38, 0x44, 0x44, 0x44, 0x38};
+const uint8_t font_data_p[] = {0xf8, 0x24, 0x24, 0x24, 0x18};
+const uint8_t font_data_q[] = {0x18, 0x24, 0x24, 0x24, 0xf8};
+const uint8_t font_data_r[] = {0x04, 0x78, 0x04, 0x04, 0x08};
+const uint8_t font_data_s[] = {0x08, 0x54, 0x54, 0x54, 0x20};
+const uint8_t font_data_t[] = {0x04, 0x3f, 0x44, 0x44, 0x20};
+const uint8_t font_data_u[] = {0x3c, 0x40, 0x40, 0x20, 0x7c};
+const uint8_t font_data_v[] = {0x1c, 0x20, 0x40, 0x20, 0x1c};
+const uint8_t font_data_w[] = {0x3c, 0x40, 0x30, 0x40, 0x3c};
+const uint8_t font_data_x[] = {0x44, 0x28, 0x10, 0x28, 0x44};
+const uint8_t font_data_y[] = {0x1c, 0xa0, 0xa0, 0xa0, 0x7c};
+const uint8_t font_data_z[] = {0x44, 0x64, 0x54, 0x4c, 0x44};
+
 constexpr Font make_font() {
     Font font = {};
     font['A'] = font_char_info{sizeof(font_data_A), font_data_A};
@@ -72,6 +99,33 @@ constexpr Font make_font() {
     font['X'] = font_char_info{sizeof(font_data_X), font_data_X};
     font['Y'] = font_char_info{sizeof(font_data_Y), font_data_Y};
     font['Z'] = font_char_info{sizeof(font_data_Z), font_data_Z};
+
+    font['a'] = font_char_info{sizeof(font_data_a), font_data_a};
+    font['b'] = font_char_info{sizeof(font_data_b), font_data_b};
+    font['c'] = font_char_info{sizeof(font_data_c), font_data_c};
+    font['d'] = font_char_info{sizeof(font_data_d), font_data_d};
+    font['e'] = font_char_info{sizeof(font_data_e), font_data_e};
+    font['f'] = font_char_info{sizeof(font_data_f), font_data_f};
+    font['g'] = font_char_info{sizeof(font_data_g), font_data_g};
+    font['h'] = font_char_info{sizeof(font_data_h), font_data_h};
+    font['i'] = font_char_info{sizeof(font_data_i), font_data_i};
+    font['j'] = font_char_info{sizeof(font_data_j), font_data_j};
+    font['k'] = font_char_info{sizeof(font_data_k), font_data_k};
+    font['l'] = font_char_info{sizeof(font_data_l), font_data_l};
+    font['m'] = font_char_info{sizeof(font_data_m), font_data_m};
+    font['n'] = font_char_info{sizeof(font_data_n), font_data_n};
+    font['o'] = font_char_info{sizeof(font_data_o), font_data_o};
+    font['p'] = font_char_info{sizeof(font_data_p), font_data_p};
+    font['q'] = font_char_info{sizeof(font_data_q), font_data_q};
+    font['r'] = font_char_info{sizeof(font_data_r), font_data_r};
+    font['s'] = font_char_info{sizeof(font_data_s), font_data_s};
+    font['t'] = font_char_info{sizeof(font_data_t), font_data_t};
+    font['u'] = font_char_info{sizeof(font_data_u), font_data_u};
+    font['v'] = font_char_info{sizeof(font_data_v), font_data_v};
+    font['w'] = font_char_info{sizeof(font_data_w), font_data_w};
+    font['x'] = font_char_info{sizeof(font_data_x), font_data_x};
+    font['y'] = font_char_info{sizeof(font_data_y), font_data_y};
+    font['z'] = font_char_info{sizeof(font_data_z), font_data_z};
     return font;
 }
 
@@ -89,7 +143,7 @@ SSD1306::SSD1306(I2cDevice &&device, gpio_num_t reset_pin)
   memset(buffer_.get(), 0x00, buffer_size());
 
   size_t offset = 0;
-  const char* const str = "ABCDEFGHIJKLMNOPQRSTU";
+  const char* str = "ABCDEFGHIJKLMNOPQRSTU";
   for (const char* p = str; *p != '\0'; ++p) {
     const char c = *p;
     memcpy(buffer_.get() + offset, font[c].data, font[c].width);
@@ -97,8 +151,24 @@ SSD1306::SSD1306(I2cDevice &&device, gpio_num_t reset_pin)
   }
 
   offset = 128;
-  const char* const str2 = "VWXYZ";
-  for (const char* p = str2; *p != '\0'; ++p) {
+  str = "VWXYZ";
+  for (const char* p = str; *p != '\0'; ++p) {
+    const char c = *p;
+    memcpy(buffer_.get() + offset, font[c].data, font[c].width);
+    offset += font[c].width + 1;
+  }
+
+  offset = 256;
+  str = "abcdefghijklmnopqrstuvw";
+  for (const char* p = str; *p != '\0'; ++p) {
+    const char c = *p;
+    memcpy(buffer_.get() + offset, font[c].data, font[c].width);
+    offset += font[c].width + 1;
+  }
+
+  offset = 128 * 3;
+  str = "xyz";
+  for (const char* p = str; *p != '\0'; ++p) {
     const char c = *p;
     memcpy(buffer_.get() + offset, font[c].data, font[c].width);
     offset += font[c].width + 1;
